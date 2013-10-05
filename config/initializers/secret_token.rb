f11057cdc68bcf1ffd1d6553a630ced3a646367d
Ashley Zhou <ashley@ashley-VirtualBox.(none)>
2013-10-05 10:12:11 -0400

    initial commit for the rail application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp2::Application.config.secret_token = 'b71442f457be0b88ad71ae07ac742ff4ca7ccdbb78bdfcabf308d0152118c15925388d1ce31105f3af6748aa626047000432ead72ac343d331793fade1e8f5f6'

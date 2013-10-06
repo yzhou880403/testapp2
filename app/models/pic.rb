7cc72474c426e497c57995c076c9548fe9d3e581
Your Name <mail@ashleyzhou.net>
2013-10-06 15:19:14 -0400

    allow image upload

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,8 @@
 class Pic < ActiveRecord::Base
-	attr_accessible :hour, :emotion, :learned
+	attr_accessible :hour, :emotion, :learned, :image
 
 
+	mount_uploader :image, ImageUploader
 	
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131005203422) do
+ActiveRecord::Schema.define(:version => 20131006183939) do
 
   create_table "pics", :force => true do |t|
     t.integer  "hour"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20131005203422) do
     t.text     "learned"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
 end
